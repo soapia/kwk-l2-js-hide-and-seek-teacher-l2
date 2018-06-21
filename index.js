fd07c2b0dd8cbab59a21a255f9cd2088846e6452
sofia ongele <sofiaongele@gmail.com>
2018-06-21 16:33:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //Code your solution here
 
 function getFirstSelector(selector) {
-  const element = document.querySelector(selector)
-  return element
+  const element = document.querySelector(selector) // find the selector in the document
+  return element // show me the element corresponding to the selector
 }
 
 function nestedTarget() {
-  const element = document.getElementById('nested').querySelector('div.target')
-  return element
+  const element = document.getElementById('nested').querySelector('div.target') // find the div with a class of target within something with an id of #nested
+  return element // show it to meeeeee
 }
 
 function deepestChild() {
-  let element = document.getElementById('grand-node')
-  let firstBorn = element.children[0]
+  let element = document.getElementById('grand-node') // find the element with an id of #grand-node
+  let firstBorn = element.children[0] // find the first item within grand node
 
-  while (firstBorn) {
-    element = firstBorn
+  while (firstBorn) { // when another child still exists
+    element = firstBorn // make the thing we're parsing through the child
     firstBorn = element.children[0]
   }
 
@@ -23,8 +23,8 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  const element = document.querySelectorAll('.ranked-list')
-  for (let key = 0, size = element.length; key < size; key++) {
+  const element = document.querySelectorAll('.ranked-list') // find the elements with a class of ranked-list
+  for (let key = 0, size = element.length; key < size; key++) { // start with the key being 0, define the size of the array, do smth when the key is less than the size, add to key each time
     let children = element[key].children
     for (let value = 0, length = children.length; value < length; value++) {
       children[value].innerHTML = parseInt(children[value].innerHTML) + n
